21d8e355e31de742bf2ace0cf799830e5438404f
Vlad Zusko <vladzvu11@gmail.com>
2022-05-07 17:30:55 +0300

    fix: fixed file config

--- src/configs/analysis.ts
+++ src/configs/analysis.ts
@@ -1,8 +1,39 @@
+import { FileSizes } from 'modules/files';
+
+export const FILE_CONFIG = {
+  SIZE: {
+    maximum: FileSizes.getKbFromMb(10),
+  },
+  DELIMITER: {
+    initial: ',',
+  },
+};
+
 export const DATA_CONFIG = {
-  maximumFileSize: 10,
+  FEATURES: {
+    minimumNumber: 1,
+    maximumNumber: 10,
+  },
+  VALUES: {
+    maximumNumber: 1,
+  },
 };
 
 export const GRADIENT_CONFIG = {
-  initialIterationsNumber: 1000,
-  initialLearningRate: 0.01,
+  ITERATIONS: {
+    minimumNumber: 1,
+    initialNumber: 1000,
+    maximumNumber: 10000,
+  },
+  LEARNING_RATE: {
+    minimum: 0.0001,
+    maximum: 10,
+  },
+};
+
+export const COST_CONFIG = {
+  LEARINGIN_RATE: {
+    minimum: 0.001,
+    maximum: 10,
+  },
 };
