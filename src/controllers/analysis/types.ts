d3dfe80c56aa9e1079da0db8d66d895344341827
Vlad Zusko <vladzvu11@gmail.com>
2022-05-08 20:57:08 +0300

    fix: fixed analysis options types

--- src/controllers/analysis/types.ts
+++ src/controllers/analysis/types.ts
@@ -1,10 +1,28 @@
+export interface IFileOptions {
+  delimiter?: string;
+}
+
+export interface IDataOptions {
+  x: number[];
+  y: number;
+  shouldNormalize?: boolean;
+}
+export interface ICostOptions {
+  normalizingRate?: number;
+  shouldNormalize?: boolean;
+}
+
+export interface IGradientOptions {
+  initialTheta?: number[] | number;
+  iterationsNumber?: number;
+  learningRate?: number;
+  shouldNormalize?: boolean;
+  normalizingRate?: number;
+}
+
 export interface IAnalysisOptions {
-  data: {
-    x: number[];
-    y: number;
-  };
-  gradient: {
-    iterationsNumber: number;
-    learningRate: number;
-  };
+  file?: IFileOptions;
+  data: IDataOptions;
+  gradient?: IGradientOptions;
+  cost?: ICostOptions;
 }
