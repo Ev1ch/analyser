7b2fe832e983e4ee12b09a10dc6ab24a2f6f4be3
Vlad Zusko <vladzvu11@gmail.com>
2022-05-08 20:24:41 +0300

    fix: fixed gradient descent normalizing

--- src/modules/analysis/gradient-descent/index.ts
+++ src/modules/analysis/gradient-descent/index.ts
@@ -2,17 +2,12 @@ import Matrix from 'modules/analysis/matrix';
 import Hypothesis from 'modules/analysis/hypothesis';
 
 class GradientDescent {
-  private hypothesis = new Hypothesis();
-
-  constructor(private iterationsNumber = 1500, private alpha = 0.01) {}
-
-  public setIterationsNumber(iterationsNumber: number) {
-    this.iterationsNumber = iterationsNumber;
-  }
-
-  public setAlpha(alpha: number) {
-    this.alpha = alpha;
-  }
+  constructor(
+    private hypothesis: Hypothesis,
+    private iterationsNumber: number,
+    private learningRate: number,
+    private normalizingRate?: number,
+  ) {}
 
   public run(Theta: Matrix, X: Matrix, Y: Matrix) {
     let currentTheta = Theta;
@@ -29,7 +24,22 @@ class GradientDescent {
     const hypothesis = this.hypothesis.calculateMatrix(Theta, X);
     const error = Matrix.substract(hypothesis, Y);
     const sum = Matrix.multiply(Matrix.transpose(X), error);
-    const k = Matrix.createNumber(sum.getHeight(), this.alpha / m);
+    const k = Matrix.createNumber(sum.getHeight(), this.learningRate / m);
+
+    if (this.normalizingRate) {
+      const normalization = Matrix.createNumber(
+        Theta.getHeight(),
+        this.normalizingRate / m,
+      );
+
+      return Matrix.substract(
+        Theta,
+        Matrix.add(
+          Matrix.multiply(k, sum),
+          Matrix.multiply(normalization, Theta),
+        ),
+      );
+    }
 
     return Matrix.substract(Theta, Matrix.multiply(k, sum));
   }
