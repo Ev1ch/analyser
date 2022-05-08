42f24f6622f2eb0a0fc4d3605655ae92c729e6f3
Vlad Zusko <vladzvu11@gmail.com>
2022-05-08 13:23:23 +0300

    feat: added files service

---
+++ src/services/files/index.ts
@@ -0,0 +1,25 @@
+import { File } from 'formidable';
+import { FILE_CONFIG } from 'configs/analysis';
+import { FilesReader } from 'modules/files';
+import { IFileOptions } from 'controllers/analysis/types';
+
+class FilesService {
+  private reader = new FilesReader();
+
+  public async getContent(file: File | undefined, options: IFileOptions) {
+    if (!file) {
+      throw new Error('File is not present');
+    }
+
+    if (file.size > FILE_CONFIG.SIZE.maximum) {
+      throw new Error('File is too large');
+    }
+
+    const preparedOptions = options.delimiter || FILE_CONFIG.DELIMITER.initial;
+    const content = await this.reader.readCsv(file.filepath, preparedOptions);
+
+    return content;
+  }
+}
+
+export default FilesService;
