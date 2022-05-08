21e55b3f7039e69f67553a726828256457f5465e
Vlad Zusko <vladzvu11@gmail.com>
2022-05-08 20:54:59 +0300

    fix: fixed analysis config

--- src/configs/analysis.ts
+++ src/configs/analysis.ts
@@ -20,20 +20,32 @@ export const DATA_CONFIG = {
 };
 
 export const GRADIENT_CONFIG = {
+  THETA: {
+    minimum: 0,
+    initial: 0,
+    maximum: 10,
+  },
   ITERATIONS: {
     minimumNumber: 1,
     initialNumber: 1000,
     maximumNumber: 10000,
   },
   LEARNING_RATE: {
-    minimum: 0.0001,
+    minimum: 0.001,
+    initial: 0.01,
+    maximum: 10,
+  },
+  NORMALIZING_RATE: {
+    minimum: 0.001,
+    initial: 0.01,
     maximum: 10,
   },
 };
 
 export const COST_CONFIG = {
-  LEARINGIN_RATE: {
+  NORMALIZING_RATE: {
     minimum: 0.001,
+    initial: 0.01,
     maximum: 10,
   },
 };
