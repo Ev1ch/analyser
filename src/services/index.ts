2b43ab07c7b47b0c12dbc1a7705af1efc3bcf0b6
Vlad Zusko <vladzvu11@gmail.com>
2022-05-08 13:25:43 +0300

    feat: added services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,4 @@
 export { default as AnalysisService } from './analysis';
+export { default as FilesService } from './files';
+export { default as OptionsService } from './options';
+export { default as DataService } from './data';
