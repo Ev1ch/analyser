311cdde7d50a2d431c508331eb04d853d23031f7
Vlad Zusko <vladzvu11@gmail.com>
2022-05-10 11:08:59 +0300

    feat: added analysis modules index file

---
+++ src/modules/analysis/index.ts
@@ -0,0 +1,6 @@
+export { default as Cost } from './cost';
+export { default as GradientDescent } from './gradient-descent';
+export { default as Hypothesis } from './hypothesis';
+export { default as Matrix } from './matrix';
+export { default as Normalization } from './normalization';
+export { default as Range } from './range';
