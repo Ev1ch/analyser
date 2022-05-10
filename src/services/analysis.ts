6d99068aefa04b6504b6d11ae54dbaa7c3adfb06
Vlad Zusko <vladzvu11@gmail.com>
2022-05-10 11:36:27 +0300

    fix: fixed settings in analysis service

--- src/services/analysis.ts
+++ src/services/analysis.ts
@@ -8,41 +8,65 @@ import {
 } from 'modules/analysis';
 import IAnalysisOptions from 'schemas/analysis/type';
 import IGradientOptions from 'schemas/gradient/type';
+import IDataOptions from 'schemas/data/type';
 
 class AnalysisService {
   public getAnalysis(data: Row[], options: IAnalysisOptions) {
     const matrix = new Matrix(data);
-    const hypothesis = new Hypothesis();
-    const gradientDescent = new GradientDescent(
-      hypothesis,
-      options.gradient?.iterationsNumber ||
-        GRADIENT_CONFIG.iterations.initialNumber,
-      options.gradient?.learningRate || GRADIENT_CONFIG.learningRate.initial,
-      options.gradient?.shouldNormalize
-        ? options.gradient?.normalizingRate ||
-          GRADIENT_CONFIG.normalizingRate.initial
-        : undefined,
-    );
+    const gradientDescent = this.getGradientDescent(options.gradient);
 
-    const X = new Matrix(matrix.getColumns(...options.data.x));
+    const X = this.getX(matrix, options.data);
+    const Y = new Matrix(matrix.getColumn(options.data.y));
+    const Theta = this.getTheta(X, options.gradient);
+
+    return gradientDescent.run(Theta, X, Y);
+  }
+
+  private getGradientDescent(options?: IGradientOptions) {
+    const hypothesis = new Hypothesis();
+    const iterationsNumber =
+      options?.iterationsNumber ?? GRADIENT_CONFIG.iterations.initialNumber;
+    const learningRate =
+      options?.learningRate ?? GRADIENT_CONFIG.learningRate.initial;
+    const normalizingRate = options?.shouldNormalize
+      ? options?.normalizingRate ?? GRADIENT_CONFIG.normalizingRate.initial
+      : undefined;
+
+    return new GradientDescent(
+      hypothesis,
+      iterationsNumber,
+      learningRate,
+      normalizingRate,
+    );
+  }
+
+  private getX(data: Matrix, options: IDataOptions) {
+    const X = new Matrix(data.getColumns(...options.x));
     const additionalColumn: Column = Array(X.getHeight()).fill([1]);
     X.preppendColumn(additionalColumn);
 
-    if (options.data.shouldNormalize) {
+    if (options.shouldNormalize) {
       const normalization = new Normalization();
       normalization.normalize(X);
     }
 
-    const Y = new Matrix(matrix.getColumn(options.data.y));
+    return X;
+  }
 
-    const thetaColumn = options.gradient?.initialTheta
-      ? Array.isArray(options.gradient.initialTheta)
-        ? options.gradient.initialTheta
-        : Array(X.getWidth()).fill(options.gradient.initialTheta)
-      : Array(X.getWidth()).fill(GRADIENT_CONFIG.theta.initial);
-    const Theta = Matrix.createVector(thetaColumn);
+  private getTheta(X: Matrix, options?: IGradientOptions) {
+    if (!options || options.initialTheta === undefined) {
+      const thetaColumn = Array(X.getWidth()).fill(
+        GRADIENT_CONFIG.theta.initial,
+      );
+      return Matrix.createVector(thetaColumn);
+    }
 
-    return gradientDescent.run(Theta, X, Y);
+    if (Array.isArray(options.initialTheta)) {
+      return Matrix.createVector(options.initialTheta);
+    }
+
+    const thetaColumn = Array(X.getWidth()).fill(options.initialTheta);
+    return Matrix.createVector(thetaColumn);
   }
 }
 
